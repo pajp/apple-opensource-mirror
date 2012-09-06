e0be8215ffddefde5416f79f2990c4e874a36e33
Rasmus Sten <rasmus.sten@f-secure.com>
2012-09-06 13:38:06 +0300

    indentation fix

--- mirror.rb
+++ mirror.rb
@@ -14,8 +14,8 @@ tmpdir = '.projects.tmp'
 releaseurl = "http://opensource.apple.com/release/#{release}/"
 
 if ARGV[0] == "--release"
-   ARGV.shift
-   release = "mac-os-x-" + ARGV.shift.gsub(/\./, "")
+  ARGV.shift
+  release = "mac-os-x-" + ARGV.shift.gsub(/\./, "")
 end
 
 releaseplist = "http://opensource.apple.com/plist/#{release}.plist"
