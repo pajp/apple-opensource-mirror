c9854f1713183d448308a6d3fea62b557f90ddf1
Rasmus Sten <rasmus@dll.nu>
2013-03-27 20:29:35 +0200

    exit with error in case of error

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
 for release; do
     ./mirror.rb --release "$release" | grep -v '^#' | xargs ./mirror.rb --release "$release"
+    [ ${PIPESTATUS[0]} -eq 0 ] && [ ${PIPESTATUS[0]} -eq 0 ]
 done
